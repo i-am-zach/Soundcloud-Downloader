7e190ceef8a6582cb9a1c763dee166599becad37
Zach <zachlefko@gmail.com>
2019-10-22 21:18:46 -0500

    Project rearranging 2.0

---
+++ soundcloud.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import subprocess
+
+
+output_path = r'/home/zach/Python/Soundcloud Downloader/temp'
+url = r'https://soundcloud.com/zach-lefkovitz/sets/dl'
+sauce = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(sauce, 'lxml')
+
+print('DONWLOADING FROM: ' + soup.title.string.upper().replace(' | FREE LISTENING ON SOUNDCLOUD', '') + '...')
+
+links = soup.findAll('a')
+
+usernameRegex = re.compile("\"/([^/]+)")
+linkRegex = re.compile("/([^/]+)/([^\"]+)")
+
+song_names = []
+usernames = []
+song_urls = []
+for link in links:
+    if 'url' in str(link):
+        if usernameRegex.search(str(link)):
+            usernames.append(usernameRegex.search(str(link)).group(1))
+        else:
+            usernames.append(None)
+        if linkRegex.search(str(link)):
+            song_urls.append(linkRegex.search(str(link)).group())
+        else:
+            song_urls.append(None)
+        song_names.append(link.string)
+
+del song_names[0]
+del usernames[0]
+del song_urls[0]
+songs = []
+for i in range(len(song_names)):
+  songs.append({
+      'name': song_names[i],
+      'username': usernames[i],
+      'url': 'https://soundcloud.com' + song_urls[i]
+  }
+)
+
+
+# scdl -l *songurl* --path *path to folder*
+for song in songs:
+    subprocess.Popen(['scdl', '-l', song['url'], '--path', output_path])
