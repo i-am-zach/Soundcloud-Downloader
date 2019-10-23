9a5d37a9bc58ffd9d8be1076a84d701bb9c16f73
Zach <zachlefko@gmail.com>
2019-10-22 20:15:25 -0500

    Scraped song urls

--- soundcloud.py
+++ soundcloud.py
@@ -12,23 +12,29 @@ print('DONWLOADING FROM: ' + soup.title.string.upper().replace(' | FREE LISTENIN
 links = soup.findAll('a')
 
 usernameRegex = re.compile("\"/([^/]+)")
+linkRegex = re.compile("/([^/]+)/([^\"]+)")
 
 song_names = []
 usernames = []
+song_urls = []
 for link in links:
     if 'url' in str(link):
         if usernameRegex.search(str(link)):
             usernames.append(usernameRegex.search(str(link)).group(1))
         else:
             usernames.append(None)
+        if linkRegex.search(str(link)):
+            song_urls.append(linkRegex.search(str(link)).group())
+        else:
+            song_urls.append(None)
         song_names.append(link.string)
 
 del song_names[0]
 del usernames[0]
+del song_urls[0]
 songs = []
 for i in range(len(song_names)):
   songs.append({song_names[i]: {
       'username': usernames[i],
-      'link': '?'
+      'url': 'soundcloud.com' + song_urls[i]
   }})
-print(songs)
